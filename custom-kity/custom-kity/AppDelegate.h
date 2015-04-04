d57f87aac4e4f8411e39fe9e0d873782664c30e6
Lance Lan <lance@192.168.3.101>
2015-04-04 13:45:41 +0800

    init project

---
+++ custom-kity/custom-kity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  custom-kity
+//
+//  Created by Lance Lan on 15/4/4.
+//  Copyright (c) 2015年 Lance Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ custom-kity/custom-kity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  custom-kity
+//
+//  Created by Lance Lan on 15/4/4.
+//  Copyright (c) 2015年 Lance Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
